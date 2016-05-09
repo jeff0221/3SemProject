3719ea8acf128736843239fcef31863f12bfcfde
Xinkai <stanley_h007@hotmail.com>
2016-05-09 10:48:40 +0200

    testClass

---
+++ src/MainRun.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Stormwind on 09/05/2016.
+ */
+public class MainRun
+{
+
+}
---
+++ test/MainRunTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Stormwind on 09/05/2016.
+ */
+public class MainRunTest
+{
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
