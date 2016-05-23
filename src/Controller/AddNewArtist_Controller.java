9dca9b56e6ea9124e13db87e4f2b3178d920b3d5
Jeffrey <jeffrey@kadnet.dk>
2016-05-23 12:55:37 +0200

    Added controller for AddNewArtist

---
+++ src/Controller/AddNewArtist_Controller.java
@@ -0,0 +1,18 @@
+package Controller;
+
+import GUI.Warning_UI;
+import Model.Artist;
+import Model.Database;
+
+/**
+ * Created by Jeffrey on 18-05-2016.
+ */
+public class AddNewArtist_Controller{
+    public void operateInsertion(String firstName, String lastName, String address, int phoneNumber,String email, String cpr, String artistName){
+        Warning_UI warning = new Warning_UI();
+        if(warning.alertWindow("Are you want to save?","Are you sure you want to create a new artist with this information?")){
+        Database.getInstance().insertArtist(new Artist(firstName,lastName,address,phoneNumber,email,cpr,artistName));
+        }
+    }
+
+}
